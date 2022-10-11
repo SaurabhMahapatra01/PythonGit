7e803446d7da99fe4162512321df4b66ec10d988
Saurabh12112002 <saurabh.12109637@lpu.in>
2022-10-11 20:30:17 +0530

    String Slicing

---
+++ 2.String/2StringSlicing.py
@@ -0,0 +1,11 @@
+# Syntax : str[start_index : end_index ]
+# Syntax : str[start_index : end_index : step.size]
+
+str = input()
+print(str)
+print(str[0:-1])
+print(str[2:10])
+print(str[:])
+print(str[0:-1:2])
+print(str[16:0:-1]) #reverse the string in limit 
+print(str[ : : -1]) #reverse whole string
