5844f48fff751e6182d83c55a4c144be0f0aba34
Saurabh12112002 <saurabh.12109637@lpu.in>
2022-10-11 20:28:39 +0530

    Create PritingPattern.py
    Printing Patterns

---
+++ 1.Control FLow/PritingPattern.py
@@ -0,0 +1,24 @@
+# Q .Program to print half pyramid using *
+
+# size = int(input())
+# for row in range(size):
+# 	for column in range (row + 1):
+# 		print("*" , end = " ")
+# 	print(end = "\n")
+
+# Q. Inverted half pyramid using *
+
+# size = int(input())
+# for row in range(size, 0, -1):
+# 	for column in range (0, row):
+# 		print("*" , end = " ")
+# 	print(end = "\n")
+
+
+# Q. Program to print half pyramid a using numbers
+
+# size = int(input())
+# for row in range(size):
+# 	for column in range (row + 1):
+# 		print(column + 1 , end = " ")
+# 	print(end = "\n")
