c26a229d8f2fac02dbfe0f43fd6dfbc5a08d297a
Saurabh12112002 <saurabh.12109637@lpu.in>
2022-10-12 19:36:45 +0530

    Introduction in Lists

---
+++ 3.Lists/1.ListINtro.py
@@ -0,0 +1,19 @@
+# lst = list()
+# lst = []  #Emty Lists
+
+list = [1,2,3,4,9,8,7,6]
+print(list)
+print(type(list))
+
+print(list[0])
+print(list[-1])
+
+print(len(list))
+
+print(list[1:-1])
+print(list[::-1])  #Reverse of list
+
+#Changing elements
+#List is Mutable
+list[0] = 10
+print(list)
