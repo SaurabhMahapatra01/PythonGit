731fd3941f7ed627200d3dc8f583d30c90f6d588
Saurabh12112002 <saurabh.12109637@lpu.in>
2022-10-12 19:35:50 +0530

    String Operations

---
+++ 2.String/4StringOperation.py
@@ -0,0 +1,7 @@
+str1 = input()
+str2 = input()
+print(str1 + str2) #Concatenation 2 of Strings
+# print(str1 + 3)  # ERROR Because of Concatenation of other data type
+
+print(str1 + str(3)) # Hold on.. type conversion is the workaround
+print(str1 * 3) #Multiplication of string with int n results into repetition string n times
