ad115859317876ce1a632e3d370514168cfbd491
Saurabh12112002 <saurabh.12109637@lpu.in>
2022-10-12 19:35:31 +0530

    String Methods

---
+++ 2.String/3StringMethods.py
@@ -0,0 +1,13 @@
+str = input()
+print(str)
+print(str.lower()) #Converting everything in Lower Case
+print(str.upper()) #Converting everything in Upper Case
+print(str.title()) #Capital First Character of each word
+print(str.find("come")) #Retruns the index of first  occurence of the word, -1 otherwise
+print(str.find("game")) # -1 when string is not present
+print(str.split()) #Splits the string with space " " and creates a list
+print(str.split('o'))
+print(str.count('o')) #Counts the number of times a string is appearing
+print(str.replace("to","in")) #Replace Original string with new string
+
+
