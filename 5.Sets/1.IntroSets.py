a8215eedeef076bc999d3fd0a895fcf146f6b658
Saurabh12112002 <saurabh.12109637@lpu.in>
2022-10-19 19:48:38 +0530

    Intro Of Sets

---
+++ 5.Sets/1.IntroSets.py
@@ -0,0 +1,16 @@
+# Contains only unique elements
+# sets are mutable
+# unordered
+# non-idexable
+
+s = set()
+print(type(s))
+
+
+# No duplicate elements
+set = {1, 2, 3, 4, 5, 6, 2, 1, 4}
+print(set)   
+set.add(7)
+print(set)
+set.remove(5)
+print(set)
