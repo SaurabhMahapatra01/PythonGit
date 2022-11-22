b8401efebfa22a8f5822b9b79aebb9ecb52e6c1f
Saurabh12112002 <saurabh.12109637@lpu.in>
2022-11-23 01:38:33 +0530

    Random Module

---
+++ 10.PythonModule/2.RandomModule.py
@@ -0,0 +1,12 @@
+import random
+
+print(dir(random))  #it shows all Random functions
+
+print(random.randint(2, 18))
+
+
+print(random.random())  #it will give random number between 0 to 1
+
+print(random.random()*20)  # it will give Random Number Between 0 to 20
+
+
